9cd96caf10ac32c7586f5dbccc9c86c288ed3a6e
MatiasTorresdL <matias.torresdl.12@gmail.com>
2025-09-19 16:45:12 -0300

    modificacion de models

--- models.py
+++ models.py
@@ -5,11 +5,9 @@ class Persona(Base):
     __tablename__ = "personas"
 
 
-    id = Column(Integer, primary_key=True, index=True)
+    dni = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, nullable=False)
     email = Column(String,unique=True, nullable=False)
-    dni = Column(Integer, unique=True, nullable=False)
     telefono = Column(String,nullable=False)
     fecha_Nacimiento = Column(Date,nullable=False)
-    edad = Column(Integer,nullable=False)
     habilitado= Column(Boolean,default=True)
