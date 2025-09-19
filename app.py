28b9f3274fcdb21f880f37e8c414126b62279c62
MatiasTorresdL <matias.torresdl.12@gmail.com>
2025-09-19 15:01:44 -0300

    Actualizar database.py y models.py con la configuración de SQLAlchemy

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
+from database import engine, Base
+import models
 
 app = FastAPI()
+
+#crear tablas al iniciar
+Base.metadata.create_all(bind=engine)
 
 @app.get("/hola_mundo")
 
 def hola_mundo():
     return "Hello World!"
 
-print ("hola grupo")
-print ("q jodido es esto")
